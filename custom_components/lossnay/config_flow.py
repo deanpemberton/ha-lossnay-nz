e7fd1550432a1d5cb716bb56ea7c2500492b305c
Dean Pemberton <dean@cassini.nz>
2024-10-28 09:43:14 +1300

    Changes to config_flow

--- custom_components/lossnay/config_flow.py
+++ custom_components/lossnay/config_flow.py
@@ -1,20 +1,29 @@
-
+import voluptuous as vol
 from homeassistant import config_entries
+from homeassistant.core import callback
+
 from .const import DOMAIN
+
 
 @config_entries.HANDLERS.register(DOMAIN)
 class LossnayConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
+    CONNECTION_CLASS = config_entries.CONN_CLASS_CLOUD_POLLING
 
     async def async_step_user(self, user_input=None):
         if user_input is None:
-            return self.async_show_form(step_id="user", data_schema=vol.Schema({
-                vol.Required("username"): str,
-                vol.Required("password"): str,
-                vol.Required("mqtt_broker"): str,
-                vol.Required("mqtt_port"): int,
-                vol.Optional("mqtt_user"): str,
-                vol.Optional("mqtt_password"): str,
-            }))
+            return self.async_show_form(
+                step_id="user",
+                data_schema=vol.Schema({
+                    vol.Required("username"): str,
+                    vol.Required("password"): str,
+                    vol.Required("app_version", default="6.3.1918"): str,
+                    vol.Required("unit_id"): str,
+                    vol.Required("mqtt_broker"): str,
+                    vol.Required("mqtt_port", default=1883): int,
+                    vol.Optional("mqtt_user"): str,
+                    vol.Optional("mqtt_password"): str
+                })
+            )
 
-        return self.async_create_entry(title="Lossnay", data=user_input)
+        return self.async_create_entry(title="Mitsubishi Lossnay", data=user_input)
